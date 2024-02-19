e637778b687313b5c85293db7f2179ba96d1bcca
Roohi <roohi.shama@gmail.com>
2024-02-20 02:03:07 +0500

    first commit, passportjs file

---
+++ example.js
@@ -0,0 +1,13 @@
+let a =0
+
+console.log("a: "+a)
+
+function func(){
+    let b=10
+    console.log("func a: "+a)
+    console.log("func b: "+b)
+}
+func()
+console.log(a)
+console.log("1" + 1);
+console.log([1, 2, 3]+"1"+"1");
